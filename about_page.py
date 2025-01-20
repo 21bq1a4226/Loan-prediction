cb9696ef0561eed59c7e4932bad0392ca280095f
21bq1a4226 <113083133+21bq1a4226@users.noreply.github.com>
2025-01-20 13:01:32 +0530

    Update about_page.py

--- about_page.py
+++ about_page.py
@@ -22,7 +22,7 @@ def about_page():
     st.markdown("---")
     st.markdown(
         """
-        <div style="text-align: center; padding: 1px; background-color: #f2c855; border-radius: 1px; border: 1.5px solid black; margin-bottom: 20px;">
+        <div style="text-align: center; padding: 1px; background-color: white; border-radius: 1px; border: 1.5px solid black; margin-bottom: 20px;">
             <p style="color: black; font-size: 20px;"><b>The Loan Prediction System aims to assist financial institutions in determining the eligibility of loan applicants by analyzing their financial and personal details. The challenge lies in accurately predicting whether a loan should be approved or rejected based on key factors such as income, credit history, employment type, loan amount, and repayment capacity. The system needs to minimize the risk for the lender while ensuring an efficient and fair approval process for the applicants.
         </div>
         """,
@@ -30,7 +30,7 @@ def about_page():
     )
     st.markdown(
         """
-        <div style="text-align: center; padding: 1px; background-color: #29f2a9; border-radius: 1px; border: 1.5px solid black; margin-bottom: 20px;">
+        <div style="text-align: center; padding: 1px; background-color: white; border-radius: 1px; border: 1.5px solid black; margin-bottom: 20px;">
             <p style="color: black; font-size: 20px;"><b>The Loan Prediction System leverages machine learning algorithms to streamline the loan approval process by automating the evaluation of applicants' eligibility. By analyzing historical loan data, the system identifies patterns and critical factors that influence loan approval decisions. The model predicts the likelihood of approval based on attributes such as income, credit history, and loan tenure. This solution not only enhances the speed and accuracy of decision-making but also reduces human bias, enabling financial institutions to make data-driven, reliable, and consistent decisions.
         </div>
         """,
@@ -38,7 +38,7 @@ def about_page():
     )
     st.markdown(
         """
-        <div style="text-align: center; padding: 1px; background-color: #3faceb; border-radius: 1px; border: 1.5px solid black; margin-bottom: 20px;">
+        <div style="text-align: center; padding: 1px; background-color: white; border-radius: 1px; border: 1.5px solid black; margin-bottom: 20px;">
             <p style="color: black; font-size: 20px;"><b>The system employs various machine learning techniques, including Logistic Regression, Decision Trees, Random Forest, Gradient Boosting (XGBoost, LightGBM), and Support Vector Machines (SVM). Data preprocessing involves handling missing values, feature encoding (e.g., One-Hot Encoding or Label Encoding), and scaling of numerical features. Feature selection techniques are used to identify the most influential variables. Performance is evaluated using metrics such as accuracy, precision, recall, F1-score, and ROC-AUC. The final model is fine-tuned using hyperparameter optimization methods like Grid Search or Random Search.   
         </div>
         """,
