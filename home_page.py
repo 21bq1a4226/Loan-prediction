682dba53e68dbbd99a72a46040184e0aba8d4f30
21bq1a4226 <113083133+21bq1a4226@users.noreply.github.com>
2025-01-20 10:12:07 +0530

    Update home_page.py

--- home_page.py
+++ home_page.py
@@ -30,7 +30,7 @@ def home_page():
     st.markdown(
         """
         <div style="text-align: center;">
-            <img src="https://media.istockphoto.com/id/508021898/video/education-expenses-loan-graduation-cap-on-smart-phone-pad-mobile.jpg?s=640x640&k=20&c=eBi43oQi4lApJzAuYDVVWOQBf0nZ3sb99fbn-pJEBhg=" style="max-width: 40%;">
+            <img src="https://miro.medium.com/v2/resize:fit:735/0*_DPvD2SGibZBsb9f.jpg" style="max-width: 40%;">
         </div>
         """,
         unsafe_allow_html=True
